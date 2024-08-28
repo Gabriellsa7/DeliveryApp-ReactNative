76c2733e7e4d8d168cb125c97ec7405b78a25cda
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-28 10:43:57 -0300

    feat: add some responsive styles

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -4,27 +4,27 @@ import PagerView from "react-native-pager-view";
 
 const Banner = () => {
   return (
-    <View className="w-full h-36 rounded-2xl mt-5 mb-4">
+    <View className="w-full h-36 md:h-60 rounded-2xl mt-5 mb-4">
       <PagerView style={{ flex: 1 }} initialPage={0} pageMargin={14}>
         <Pressable
-          className="w-full h-36 rounded-2xl"
+          className="w-full h-36 rounded-2xl md:h-60"
           key="1"
           onPress={() => console.log("Clicou no banner 1")}
         >
           <Image
             source={require("../../assets/banner1.png")}
-            className="w-full h-36 rounded-2xl"
+            className="w-full h-36 md:h-60 rounded-2xl"
           />
         </Pressable>
 
         <Pressable
-          className="w-full h-36 rounded-2xl"
+          className="w-full h-36 md:h-60 rounded-2xl"
           key="2"
           onPress={() => console.log("Clicou no banner 1")}
         >
           <Image
             source={require("../../assets/banner2.png")}
-            className="w-full h-36 rounded-2xl"
+            className="w-full h-36 md:h-60 rounded-2xl"
           />
         </Pressable>
       </PagerView>

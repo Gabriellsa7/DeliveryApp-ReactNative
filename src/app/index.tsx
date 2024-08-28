e25762271f14293f9466ce471533878f28d670a3
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-28 00:25:45 -0300

    feat: add banner component and some comments

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,23 @@
 import { ScrollView, Text, View } from "react-native";
 import Header from "../components/header";
 import Constants from "expo-constants";
+import Banner from "../components/banner";
 
 const statusBarHeight = Constants.statusBarHeight;
 
 export default function Index() {
   return (
+    // used scroll view because you can scroll the screen
     <ScrollView
       style={{ flex: 1 }}
       className="bg-slate-200"
       showsVerticalScrollIndicator={false}
     >
       <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8 }}>
+        {/* import header component */}
         <Header />
+        {/* import banner component */}
+        <Banner />
       </View>
     </ScrollView>
   );

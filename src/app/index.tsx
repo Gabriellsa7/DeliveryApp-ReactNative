88cf3205e1f64df088da0e8eec28cf26936e6f04
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-28 10:42:49 -0300

    feat: add more 2 components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,14 @@ import { ScrollView, Text, View } from "react-native";
 import Header from "../components/header";
 import Constants from "expo-constants";
 import Banner from "../components/banner";
+import Search from "../components/search";
+import Section from "../components/section";
+import { useRouter } from "expo-router";
 
 const statusBarHeight = Constants.statusBarHeight;
 
-export default function Index() {
+export default function Home() {
+  const router = useRouter();
   return (
     // used scroll view because you can scroll the screen
     <ScrollView
@@ -18,6 +22,15 @@ export default function Index() {
         <Header />
         {/* import banner component */}
         <Banner />
+        {/* import search component */}
+        <Search />
+        {/* import section component */}
+        <Section
+          name="Trending foods"
+          label="See More"
+          action={() => router.push("/see-more")}
+          size="text-2xl"
+        />
       </View>
     </ScrollView>
   );

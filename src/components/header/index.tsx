d32c799bb52e96edc03cd5cf6ea53485be8afaa4
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-28 00:27:10 -0300

    feat: created the header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,9 +4,21 @@ import { Ionicons, Feather } from "@expo/vector-icons";
 
 const Header = () => {
   return (
-    <View>
-      <Pressable className="">
+    <View className=" flex flex-row w-full items-center justify-between">
+      <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
         <Ionicons name="menu" size={20} color="#121212" />
+      </Pressable>
+
+      <View className="flex flex-col items-center justify-center">
+        <Text className="text-center text-sm text-slate-800">Localization</Text>
+        <View className="flex-row items-center justify-center gap-1">
+          <Feather name="map-pin" size={14} color="#FF0000" />
+          <Text className="text-lg font-bold">Salgado</Text>
+        </View>
+      </View>
+
+      <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+        <Feather name="bell" size={20} color="#121212" />
       </Pressable>
     </View>
   );

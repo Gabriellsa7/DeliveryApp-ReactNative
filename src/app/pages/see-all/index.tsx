2a863b7947eeed0ac99701b8323491f3038e3e73
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-28 13:31:11 -0300

    feat: created the see all restaurants

---
+++ src/app/pages/see-all/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SeeAll = () => {
+  return (
+    <View>
+      <Text>SeeAll</Text>
+    </View>
+  );
+};
+
+export default SeeAll;
